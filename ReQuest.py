6666226dbbdd897daa5093396d1f651c72e0a91a
ReQuest-iwind <g.serafeim@iwind.gr>
2025-03-01 23:59:01 +0200

    Update ReQuest.py

--- ReQuest.py
+++ ReQuest.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+with open('./publices.txt','r') as f:
+    data = [i.split() for i in f]
+Members = [str(data[i][0])+' '+str(data[i][1]) for i in range(1,len(data))]
+Publics = [float(data[i][2]) for i in range(1,len(data))]
+Colors  = [str(data[i][3]) for i in range(1,len(data))]
+
 st.set_page_config(page_title='ReQuest',page_icon=":chart_with_upwards_trend:",layout="wide")
 
 with st.container():
@@ -8,3 +14,12 @@ with st.container():
         st.header('**:blue[ReQuest]**',divider='blue')
     with col2:
         image = st.image('./logo.png',width=200)
+
+col1,col2 = st.columns([1,1])
+
+with col1:
+    with st.form(key='upload'):
+        st.subheader(':blue[PDF uploading]')
+        member = st.selectbox(':blue[Who are you?]',options=['--- None ---',]+Members)
+        upload = st.file_uploader(':blue[Upload your pdf file!]',type=['pdf'])
+        submit = st.form_submit_button(':blue[Sumbit]',use_container_width=True)
