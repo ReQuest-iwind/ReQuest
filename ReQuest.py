35cbbbeed90dc81cc551c3121e5ee4017464e137
ReQuest-iwind <g.serafeim@iwind.gr>
2025-03-01 23:52:46 +0200

    Update ReQuest.py

--- ReQuest.py
+++ ReQuest.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title='ReQuest',page_icon=":chart_with_upwards_trend:",l
 
 with st.container():
     col1,col2 = st.columns([7,1])
-        with col1:
-          st.header('**:blue[ReQuest]**',divider='blue')
-        with col2:
-          image = st.image('./logo.png',width=200)
+    with col1:
+        st.header('**:blue[ReQuest]**',divider='blue')
+    with col2:
+        image = st.image('./logo.png',width=200)
