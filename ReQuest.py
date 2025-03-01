ff31b342d3403e1a561f485fd7ac80d09e655225
ReQuest-iwind <g.serafeim@iwind.gr>
2025-03-01 23:50:50 +0200

    Update ReQuest.py

--- ReQuest.py
+++ ReQuest.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write('Giannis Serafeim!')
+st.set_page_config(page_title='ReQuest',page_icon=":chart_with_upwards_trend:",layout="wide")
+
+with st.container():
+    col1,col2 = st.columns([7,1])
+        with col1:
+          st.header('**:blue[ReQuest]**',divider='blue')
+        with col2:
+          image = st.image('./logo.png',width=200)
